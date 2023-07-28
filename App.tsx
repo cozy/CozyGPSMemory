2cb27b403425515e177991816c69ded0dcd3e846
AdrienB <56419448+DaAdri@users.noreply.github.com>
2023-07-28 16:14:15 +1000

    First commit

---
+++ App.tsx
@@ -0,0 +1,251 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import type { PropsWithChildren } from 'react';
+
+import {
+  Button,
+  Modal,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Switch,
+  Text,
+  TextBase,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import BackgroundGeolocation, {
+  State,
+  Config,
+  Location,
+  LocationError,
+  Geofence,
+  GeofenceEvent,
+  GeofencesChangeEvent,
+  HeartbeatEvent,
+  HttpEvent,
+  MotionActivityEvent,
+  MotionChangeEvent,
+  ProviderChangeEvent,
+  ConnectivityChangeEvent,
+  Subscription
+} from "react-native-background-geolocation";
+
+import { UploadData, _storeId, _getId, _storeToken, _getToken, ClearAllGeolocationData, UpdateId, GeolocationSwitch } from "./EMissionCompatibility.js"
+
+const devMode = true;
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [PopUpVisible, setPopUpVisible] = useState(false);
+  const [idInputPopupVisible, setIdInputPopupVisible] = useState(false);
+
+  const UploadMobility = async (force: boolean) => {
+    await UploadData(force);
+    BackgroundGeolocation.getLocations().then(async (locations) => {
+      if (locations.length > 0) { // Méthode grossière, mais ça résout le problème
+        // Ne marchera pas si on ne détruit pas les locations avec SendData(true)
+        MakePopup("❌ There are still local positions");
+      } else {
+        MakePopup("✅ All mobility measures uploaded!");
+      }
+    })
+  };
+
+  const closeModal = () => {
+    setPopUpVisible(false);
+  };
+
+  const closeIdInputPopup = () => {
+    setIdInputPopupVisible(false);
+  };
+
+  const [IdBoxText, setIdBoxTest] = useState('');
+
+  const [myText, setMyText] = useState("No message");
+  const PopUp = () => {
+    return (
+      <Modal
+        visible={PopUpVisible}
+        animationType='fade'
+        transparent={true}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{myText}</Text>
+            <Button title="Close" onPress={closeModal} />
+          </View>
+        </View>
+      </Modal>
+    )
+  }
+
+
+  function MakePopup(text: string) {
+    setMyText(text);
+    setPopUpVisible(true);
+  }
+
+  async function UpdateIdFromButton(newId: string) {
+    console.log("Updating Id to", newId);
+    try {
+      MakePopup(await UpdateId(newId));
+    } catch (error) {
+      console.log(error);
+      MakePopup("Error: Couldn't update Id");
+    }
+
+  }
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          paddingVertical: 200,
+          justifyContent: 'center',
+          alignContent: 'center'
+        }}>
+
+        <GeolocationSwitch></GeolocationSwitch>
+
+        <Button
+          onPress={async function () {
+            let idToCopy = await _getId();
+            if (idToCopy == undefined) {
+              idToCopy = "undefinedId";
+            }
+            Clipboard.setString(idToCopy);
+          }}
+          title="Copy secret Tracker Id (for konnector)"
+        />
+
+        <Button
+          onPress={async function () {
+            setIdInputPopupVisible(true);
+          }}
+          title="Manually redefine secret Tracker Id"
+          disabled={!devMode}
+        />
+
+        <Button
+          onPress={() => { UploadMobility(true); }}
+          title="Force upload pending tracks to E-Mission"
+          disabled={!devMode}
+        />
+
+        <Button
+          onPress={async function () {
+            try {
+              await ClearAllGeolocationData();
+              MakePopup("Deleted everything");
+            } catch (error) {
+              MakePopup("Couldn't purge\n" + error)
+            }
+          }}
+          title="Purge all local data"
+          disabled={!devMode}
+        />
+
+        <PopUp></PopUp>
+
+
+
+        <Modal
+          visible={idInputPopupVisible}
+          animationType='fade'
+          transparent={true}
+          onRequestClose={closeIdInputPopup}
+          style={{ alignContent: 'center', justifyContent: 'center' }}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={{ fontSize: 24, textAlign: 'center' }}>New Id</Text>
+              <TextInput
+                autoFocus={true}
+                value={IdBoxText}
+                onChangeText={setIdBoxTest}
+                selectTextOnFocus={true}
+                keyboardType='email-address'
+                style={{
+                  padding: 20,
+                  alignContent: 'center',
+                  justifyContent: 'center',
+                  textAlign: 'center',
+                  fontSize: 18,
+                }}
+              />
+              <Button title="Cancel (keep existing Id)" onPress={async function () {
+                closeIdInputPopup();
+              }
+              } />
+              <Button title="Validate" onPress={async function () {
+                closeIdInputPopup();
+                UpdateIdFromButton(IdBoxText);
+              }
+              } />
+            </View>
+          </View>
+        </Modal>
+
+      </View >
+    </SafeAreaView >
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    alignContent: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 8,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+    textAlign: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
---
+++ EMissionCompatibility.js
@@ -0,0 +1,603 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import BackgroundGeolocation from 'react-native-background-geolocation';
+import { Platform } from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+import React, { useState } from 'react';
+import { getUniqueId, getManufacturer } from 'react-native-device-info';
+
+import {
+	Button,
+	Modal,
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Switch,
+	Text,
+	TextBase,
+	TextInput,
+	useColorScheme,
+	View,
+} from 'react-native';
+
+const DestroyLocalOnSuccess = true;
+const stopTimeoutMin = 10;
+const stopTimeoutDetectionMill = 0.8 * stopTimeoutMin * 60 * 1000; // Réduit pour prendre en compte les inexactitudes du déclenchement de l'arrêt (sinon on pourrait ne pas détecter de gap parce qu'on est arrêté depuis 9 min au lieu de 10)
+const retryOnFailTime = 15 * 60 * 1000;
+const serverURL = 'https://openpath.cozycloud.cc';
+const batch_size = 1000;
+
+
+async function _storeFlagFailUpload(Flag) {
+	try {
+		await AsyncStorage.setItem('FlagFailUpload', Flag ? 'true' : 'false');
+	} catch (error) {
+		console.log('WTF error, problème d écriture/lecture du stockage ?', error);
+		throw (error);
+	}
+};
+
+async function _getFlagFailUpload() {
+	try {
+		let value = await AsyncStorage.getItem('FlagFailUpload');
+		if (value == undefined) {
+			value = false;
+			_storeFlagFailUpload(value);
+		}
+		return !(value == 'false'); // Si undefined malgré tout on considère erreur
+	} catch (error) {
+		console.log('WTF error, problème d écriture/lecture du stockage ?', error);
+		throw (error);
+	}
+}
+
+export async function _storeId(Id) {
+	try {
+		await AsyncStorage.setItem('Id', Id);
+	} catch (error) {
+		throw (error);
+	}
+};
+
+export async function _getId() {
+	try {
+		let value = await AsyncStorage.getItem('Id');
+		if (value == undefined) {
+			await _storeId(await getUniqueId()); // Device ID par défaut
+		}
+		value = await AsyncStorage.getItem('Id'); // On s'assure que c'est enregistré
+		return value;
+	} catch (error) {
+		console.log('WTF error, problème d écriture/lecture du stockage ?', error);
+		throw (error);
+	}
+};
+
+
+export async function _storeStops(stops) {
+	try {
+		await AsyncStorage.setItem('stops', JSON.stringify(stops));
+	} catch (error) {
+		throw (error);
+	}
+};
+
+export async function _getStops() {
+	try {
+		let value = await AsyncStorage.getItem('stops');
+		if (value == null) {
+			value = [];
+		} else {
+			value = JSON.parse(value);
+		}
+		return value;
+	} catch (error) {
+		console.log('WTF error, problème d écriture/lecture du stockage ?', error);
+		throw (error);
+	}
+};
+
+export async function _storeToken(Id) {
+	try {
+		await AsyncStorage.setItem(
+			'Token',
+			Id,
+		);
+	} catch (error) {
+		console.log('WTF error, problème d écriture/lecture du stockage ?', error);
+		throw (error);
+	}
+};
+
+export async function _getToken() {
+	try {
+		let value = await AsyncStorage.getItem('Token');
+		return value;
+	} catch (error) {
+		console.log('WTF error, problème d écriture/lecture du stockage ?', error);
+		throw (error);
+	}
+}
+
+export async function ClearAllGeolocationData() {
+	// Pas testé
+	await AsyncStorage.multiRemove(['Id', 'Token', 'FlagFailUpload', 'should_be_tracking', 'stops']);
+	await BackgroundGeolocation.destroyLocations();
+}
+
+async function GenerateToken(user) {
+	let newToken = undefined;
+	try {
+		let response = await fetch(serverURL + '/profile/create', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ 'user': user }),
+		})
+		// console.log(response);
+		if (!response.ok) {
+			console.log('Error getting uuid:', response.status, response.statusText);
+
+		} else {
+
+			jsonTokenResponse = await response.json()
+			console.log('Success getting uuid for', user, ':', jsonTokenResponse['uuid']);
+			newToken = jsonTokenResponse['uuid'];
+		}
+
+	} catch (message) {
+		console.log('Error contacting server to get uuid', message);
+	}
+	return newToken;
+}
+
+async function RegisterStopNow() {
+	stops = await _getStops();
+	let new_stop = Math.floor(Date.now() / 1000);
+	console.log("Adding stop:", new_stop);
+	stops.push(new_stop);
+	await _storeStops(stops);
+}
+
+function parseISOString(ISOString) {
+	let b = ISOString.split(/\D+/);
+	return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+}
+
+function TranslateToEMissionLocationPoint(location_point) {
+	let ts = Math.floor(parseISOString(location_point['timestamp']).getTime() / 1000)
+	return {
+		"data": {
+			"accuracy": location_point['coords']['accuracy'],
+			"altitude": location_point['coords']['altitude'],
+			"bearing": location_point['coords']['heading'],
+			"filter": "distance",
+			"floor": 0,
+			"fmt_time": location_point['timestamp'],
+			"latitude": location_point['coords']['latitude'],
+			"longitude": location_point['coords']['longitude'],
+			"sensed_speed": location_point['coords']['speed'],
+			"ts": ts,
+			"vaccuracy": location_point['coords']['altitude_accuracy']
+		},
+		"metadata": {
+			"platform": "ios",
+			"write_ts": ts,
+			"time_zone": "UTC",
+			"key": "background/location",
+			"read_ts": 0,
+			"type": "sensor-data"
+		}
+	}
+}
+
+
+function TranslateToEMissionMotionActivityPoint(location) {
+	let ts = Math.floor(parseISOString(location['timestamp']).getTime() / 1000);
+	if (Platform.OS === 'ios') {
+		return {
+
+			'data': {
+				'cycling': location['activity']['type'] == 'on_bicycle',
+				'walking': location['activity']['type'] == 'on_foot',
+				'running': location['activity']['type'] == 'running',
+				'automotive': location['activity']['type'] == 'in_vehicle', // Stationary et automotive sont sensés être compatibles sur ios
+				'stationary': location['activity']['type'] == 'still',
+				'confidence': location["activity"]['confidence'],
+				'fmt_time': location['timestamp'],
+				'ts': ts,
+				'confidence_level': 'high',
+			},
+			'metadata': {
+				'write_ts': ts,
+				'time_zone': 'UTC',
+				'platform': 'ios',
+				'key': 'background/motion_activity',
+				'read_ts': 0,
+				'type': 'sensor-data'
+			}
+		};
+	}
+}
+
+
+function TranslateToEMissionRequest(locations, user) {
+	let output = {
+		'user': user,
+		'phone_to_server': [],
+	};
+	// Clipboard.setString(JSON.stringify(locations));
+	for (let translationIndex = 0; translationIndex < locations.length; translationIndex++) {
+		if (locations[translationIndex]['error'] == undefined) { //Si le points n'est pas {'error': 0}
+			//console.log("location", translationIndex, "of", locations.length);
+			const point = locations[translationIndex];
+			let ts = parseISOString(point['timestamp']);
+			let newLocationPoint = TranslateToEMissionLocationPoint(point, ts);
+
+			//console.log(newLocationPoint);
+			output['phone_to_server'].push(newLocationPoint);
+
+			if (point['coords']['accuracy'] < 200) {
+				// point identique avec clé différente
+				let newFilteredLocationPoint = TranslateToEMissionLocationPoint(point, ts);
+				newFilteredLocationPoint['metadata']['key'] = 'background/filtered_location';
+				newFilteredLocationPoint['metadata']['write_ts'] += 0.1; //Sinon bud du serveur à cause de même rite_ts je pense
+				//console.log((newFilteredLocationPoint));
+				output['phone_to_server'].push(newFilteredLocationPoint);
+			}
+			//let newMotionActivityPoint = TranslateToEMissionMotionActivityPoint(point['activity'], ts, uuids[2]);
+			//newMotionActivityPoint['metadata']['write_ts'] += 0.2;
+			//output['phone_to_server'].push(newMotionActivityPoint);
+
+			// console.log(p['uuid'], newLocationPoint['_id']['$oid'], newFilteredLocationPoint['_id']['$oid'], newMotionActivityPoint['_id']['$oid']);
+		}
+
+	}
+	return output;
+}
+
+export async function UpdateToken() {
+	try {
+		let newId = await _getId();
+		console.log('Generating token for:', newId);
+
+		let newToken = await GenerateToken(newId);
+
+		if (newToken != undefined) {
+			await _storeToken(newToken);
+			console.log('Token updated');
+
+		} else {
+			// Typiquement si pas de connexion, l'ancien token/uuid a été supprimé donc sera réessayé
+			console.log('Token couldnt be updated right now');
+		}
+	} catch (error) {
+		console.log('WTF error, problème d écriture/lecture du stockage ?', error);
+	}
+}
+
+export async function UpdateId(newId) {
+	if (newId.length > 2 && newId != await _getId()) {
+		await AsyncStorage.multiRemove(['Id', 'Token']); // Pour l'instant, ne remove pas les locations etc
+		await _storeId(newId);
+		return ('Id updated, token deleted');
+	} else {
+		return ('Same Id or invalid Id');
+	}
+}
+
+
+function Transition(state, transition, transition_ts) {
+	return ({
+		"data": {
+
+			"currState": state,
+			"transition": transition,
+			"ts": transition_ts
+
+		},
+		"metadata": {
+			"platform": "ios",
+			"write_ts": transition_ts,
+			"time_zone": "UTC",
+			"key": "statemachine/transition",
+			"read_ts": 0,
+			"type": "message"
+		}
+	});
+}
+
+async function SmartSend(locations, user, force = false) {
+	if (force) {
+		//Si on force l'upload, marquer un arrêt maintenant
+		await RegisterStopNow();
+	}
+	let stops = await _getStops();
+	// console.log("Stops:", JSON.stringify(stops));
+
+	if (stops.length > 0 && locations.length > 0) {
+		let lastStop = stops.at(-1);
+		console.log("Uploading up to:", lastStop);
+		let phone_to_server = [[]];
+		let phone_to_serverIndex = 0;
+
+		phone_to_server[phone_to_serverIndex].push(Transition("STATE_WAITING_FOR_TRIP_START", "T_EXITED_GEOFENCE", Math.floor(parseISOString(locations[0]["timestamp"]).getTime() / 1000) - 3));
+		phone_to_server[phone_to_serverIndex].push(Transition("STATE_WAITING_FOR_TRIP_START", "T_TRIP_STARTED", Math.floor(parseISOString(locations[0]["timestamp"]).getTime() / 1000) - 2));
+		phone_to_server[phone_to_serverIndex].push(Transition("STATE_ONGOING_TRIP", "T_TRIP_STARTED", Math.floor(parseISOString(locations[0]["timestamp"]).getTime() / 1000) - 1));
+
+		let index = 0;
+		while (index < locations.length && Math.floor(parseISOString(locations[index]["timestamp"]).getTime() / 1000) <= lastStop) {
+			phone_to_server[phone_to_serverIndex].push(TranslateToEMissionLocationPoint(locations[index]));
+
+			//Condition de filtered_location:
+			if (locations[index]['coords']['accuracy'] <= 200) { // Précision suffisante
+				if (index == 0 || (locations[index]['coords']['longitude'] != locations[index - 1]['coords']['longitude'] || locations[index]['coords']['latitude'] != locations[index - 1]['coords']['latitude'])) { // Différent du point précédent (check si on est au premier))
+					phone_to_server[phone_to_serverIndex].push(TranslateToEMissionLocationPoint(locations[index]));
+					phone_to_server[phone_to_serverIndex].at(-1)["metadata"]["key"] = "background/filtered_location";
+				}
+			}
+
+			//phone_to_server[phone_to_serverIndex].push(TranslateToEMissionMotionActivityPoint(locations[index]));
+
+			index++;
+			if (phone_to_server[phone_to_serverIndex].length > batch_size) {
+				phone_to_server.push([]);
+				phone_to_serverIndex++;
+			}
+		}
+
+		phone_to_server.at(-1).push(Transition("STATE_ONGOING_TRIP", "T_TRIP_END_DETECTED", lastStop + 0.1));
+		phone_to_server.at(-1).push(Transition("STATE_ONGOING_TRIP", "T_END_TRIP_TRACKING", lastStop + 0.2));
+		phone_to_server.at(-1).push(Transition("STATE_ONGOING_TRIP", "T_TRIP_ENDED", lastStop + 0.3));
+		phone_to_server.at(-1).push(Transition("STATE_WAITING_FOR_TRIP_START", "T_NOP", lastStop + 0.4));
+		//STOPPED_MOVING donne une erreur, ios ? plus utilisé ?
+
+		for (let batchIndex = 0; batchIndex < phone_to_server.length; batchIndex++) {
+			let JsonRequest = {
+				'user': user,
+				'phone_to_server': phone_to_server[batchIndex]
+			}
+			Clipboard.setString(JSON.stringify(JsonRequest));
+
+			let response = await fetch(serverURL + '/usercache/put', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(JsonRequest),
+			})
+			if (!response.ok) {
+				throw new Error(String('Error in request response:', response.status, response.statusText, await response.text()));
+
+			} else {
+				console.log('Success uploading');
+				if (DestroyLocalOnSuccess && batchIndex == phone_to_server.length - 1) {
+					console.log('Destroying local location records');
+					BackgroundGeolocation.destroyLocations();
+					//TODO proprement
+
+				}
+			}
+		}
+	} else { console.log("Pas de stops et non forcé ou pas de points; pas d'upload"); }
+
+	//TODO : nettoyer les stops de la mémoire solide
+
+	/*
+			let endIndex = locations.length - 1
+			while (endIndex > 0 && locations[endIndex]['timestamp'] > lastStop) {
+				endIndex--;
+			}
+
+			console.log('Uploading all locations until location', endIndex + 1, 'of', locations.length);
+
+			console.log('Starting batch by batch upload with', batch_size, 'locations/batch');
+			let indexUpload = 0;
+			while (indexUpload < endIndex) {
+				let slicedLocations = locations.slice(indexUpload, Math.min(indexUpload + batch_size, endIndex + 1));
+
+				let jsonData = TranslateToEMissionRequest(slicedLocations, user);
+				jsonData['phone_to_server'] = jsonData['phone_to_server'].concat(JSONStops);
+
+				Clipboard.setString(JSON.stringify(jsonData));
+
+
+
+				let response = await fetch(serverURL + '/usercache/put', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(jsonData),
+				})
+
+
+				if (!response.ok) {
+					throw new Error(String('Error in request response:', response.status, response.statusText, await response.text()));
+
+				} else {
+					console.log('Success uploading a batch');
+					indexUpload += batch_size;
+					if (DestroyLocalOnSuccess) {
+						console.log('Destroying local location records of this batch');
+
+						for (let DeleteIndex = 0; DeleteIndex < slicedLocations.length; DeleteIndex++) {
+							BackgroundGeolocation.destroyLocation(slicedLocations[DeleteIndex]['uuid']);
+						}
+
+					}
+				}
+			}
+			AsyncStorage.removeItem('stops');
+		} else {
+			console.log('No stops detected');
+		}
+	*/
+
+}
+
+export async function UploadData(force = false) { // WARNING: le message qui prévient le user que l'upload a fail se base sur le fait qu'il reste des locations localement ou non
+
+	console.log('Starting upload process');
+
+	try {
+		let locations = await BackgroundGeolocation.getLocations();
+		if (locations.length > 0) {
+			// console.log(locations);
+
+			let user = await _getId();
+			let token = await _getToken();
+			console.log('Using Id:', user);
+			if (token == null) {
+				console.log('No token, getting one...');
+				await UpdateToken();
+				console.log('done');
+				token = await _getToken();
+			}
+
+			if (token != null) {
+				console.log('Got token:', token);
+				try {
+					await SmartSend(locations, user, force);
+				} catch (message) {
+					console.log('Error trying to send data:', message);
+					if (!(await _getFlagFailUpload())) {
+						console.log('First fail, trying again in', retryOnFailTime);
+						_storeFlagFailUpload(true);
+						setTimeout(async () => {
+							if (await _getFlagFailUpload()) { // On vérifie que l'upload n'a pas marché depuis
+								console.log('Second attempt at uploading (fail', retryOnFailTime, 'ago)');
+								try {
+									await UploadData(false);
+								} catch { console.log('Failed again'); }
+							} else {
+								console.log('Cancelling second attempt, succeeded since');
+							}
+						}, retryOnFailTime)
+					} else { console.log('Already failed twice, no more attempt until event'); }
+				}
+			} else {
+				console.log('No token after trying to update it: cancelling upload');
+			}
+		} else {
+			console.log('No locations to upload');
+		}
+
+	} catch (error) {
+		throw new Error(error); //Flag et tout
+	}
+}
+
+export function GeolocationSwitch() {
+	const [enabled, setEnabled] = React.useState(false);
+	const [location, setLocation] = React.useState('');
+
+	AsyncStorage.getItem('should_be_tracking').then((shouldBeTracking) => {
+		// Aparemment se déclenche plus souvent que je voudrais, mais pas critique
+		// console.log(shouldBeTracking)
+		if (shouldBeTracking == undefined) {
+			AsyncStorage.setItem('should_be_tracking',
+				enabled ? 'true' : 'false');
+		} else {
+			setEnabled(shouldBeTracking == 'true');
+		}
+	}
+	)
+
+	React.useEffect(() => {
+		/// 1.  Subscribe to events.
+
+		const onLocation = BackgroundGeolocation.onLocation((location) => {
+			console.log('[onLocation]');
+			setLocation(JSON.stringify(location, null, 2));
+		})
+
+		const onMotionChange = BackgroundGeolocation.onMotionChange(async (event) => {
+			console.log('[onMotionChange]', event);
+			if (!event.isMoving) {
+				await RegisterStopNow();
+
+			}
+			UploadData();
+
+		});
+
+		const onActivityChange = BackgroundGeolocation.onActivityChange((event) => {
+			console.log('[onActivityChange]', event);
+		})
+
+		const onProviderChange = BackgroundGeolocation.onProviderChange((event) => {
+			console.log('[onProviderChange]', event);
+		})
+
+		const onConnectivityChange = BackgroundGeolocation.onConnectivityChange(async (event) => {
+			// ne trigger pas en emul ios, donne event = {'connected': false}
+			// à tester en réel
+			console.log('[onConnectivityChange]', event);
+			if (event.connected && await _getFlagFailUpload()) {
+				UploadData(false);
+			}
+		})
+
+		/// 2. ready the plugin.
+		BackgroundGeolocation.ready({
+			// Geolocation Config
+			desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+			showsBackgroundLocationIndicator: false, //Displays a blue pill on the iOS status bar when the location services are in use in the background (if the app doesn't have 'always' permission, the blue pill will always appear when location services are in use while the app isn't focused)
+			distanceFilter: 10,
+			stationaryRadius: 25, //Minimum, but still usually takes 200m
+			// Activity Recognition
+			stopTimeout: stopTimeoutMin,
+			// Application config
+			debug: false, // <-- enable this hear sounds for background-geolocation life-cycle and notifications
+			logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+			stopOnTerminate: false,   // <-- Allow the background-service to continue tracking when user closes the app. (not on iOS)
+			startOnBoot: true,        // <-- Auto start tracking when device is powered-up.
+			// HTTP / SQLite config
+
+			batchSync: false,       // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+			autoSync: false,         // <-- [Default: true] Set true to sync each location to server as it arrives.
+		}).then((state) => {
+			setEnabled(state.enabled);
+
+			console.log('- BackgroundGeolocation is configured and ready: ', state.enabled);
+		});
+
+		return () => {
+			// Remove BackgroundGeolocation event-subscribers when the View is removed or refreshed
+			// during development live-reload.  Without this, event-listeners will accumulate with
+			// each refresh during live-reload.
+			onLocation.remove();
+			onMotionChange.remove();
+			onActivityChange.remove();
+			onProviderChange.remove();
+		}
+	}, []);
+
+	/// 3. start / stop BackgroundGeolocation
+
+	React.useEffect(() => {
+		if (enabled) {
+			console.log('Enabling tracking');
+			BackgroundGeolocation.start();
+		} else {
+			console.log('Disabling tracking');
+			BackgroundGeolocation.stop();
+			setLocation('');
+		}
+	}, [enabled]);
+
+	return (
+		<View style={{ alignItems: 'center', padding: 50 }}>
+			<Text style={{
+				fontSize: 36, padding: 10, color: useColorScheme() === 'dark' ? '#ffffff' : '#000000',
+			}}>Tracking</Text>
+			<Switch value={enabled} onValueChange={(value) => {
+				setEnabled(value);
+				AsyncStorage.setItem('should_be_tracking', value ? 'true' : 'false');
+			}} />
+
+		</View>
+	)
+}
