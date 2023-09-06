ab994e33a7d126a50eab79454af7646fdf07d454
paultranvan <paul@cozycloud.cc>
2023-09-06 11:02:49 +0200

    fix: Auto-upload
    This fixes the auto-upload by a better handling of motion change:
    - Same behaviour between iOS and Android
    - Works for both headless and foreground

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 import BackgroundGeolocation from 'react-native-background-geolocation';
-import { UploadData, _getId, _getLog, CozyGPSMemoryLog, _getFlagFailUpload } from './EMissionCompatibility.js'
+import {
+  Log,
+  handleMotionChange,
+  handleConnectivityChange,
+} from './EMissionCompatibility.js';
 
 AppRegistry.registerComponent(appName, () => App);
-////
-// Define your Headless task -- simply a javascript async function to receive
-// events from BackgroundGeolocation:
-//
-let HeadlessTask = async (event) => {
-	let params = event.params;
-	console.log('[BackgroundGeolocation HeadlessTask] -', event.name, params);
 
-	switch (event.name) {
-		case 'motion':
-			await CozyGPSMemoryLog('State change: ' + (params.isMoving ? 'Started moving' : 'Stopped'));
-			if (!params.isMoving) {
-				await CozyGPSMemoryLog('Auto uploading from stop');
-				await UploadData();
-			}
-			break;
-		case 'connectivity':
-			await CozyGPSMemoryLog('Connectivity change to: ' + params.connected);
-			if (params.connected && await _getFlagFailUpload()) {
-				await CozyGPSMemoryLog('Auto uploading from reconnection and failed last attempt');
-				await UploadData();
-			}
-	}
-}
+const HeadlessTask = async event => {
+  const params = event.params;
+  Log('headless event name: ' + event.name);
 
+  switch (event.name) {
+    case 'location':
+      Log('[LOCATION] -' + JSON.stringify(params));
+      break;
+    case 'motionchange':
+      await handleMotionChange(params);
+      break;
+    case 'activitychange':
+      Log('[ACTIVITYCHANGE] -' + JSON.stringify(params));
+      break;
+    case 'connectivitychange':
+      await handleConnectivityChange(params);
+      break;
+    default:
+      break;
+  }
+};
 
-
-CozyGPSMemoryLog('start index.js');
-
-////
-// Register your HeadlessTask with BackgroundGeolocation plugin.
-//
 BackgroundGeolocation.registerHeadlessTask(HeadlessTask);
