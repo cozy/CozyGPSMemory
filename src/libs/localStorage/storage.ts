667c0a65367444043dd6848793c3bb1666f944de
Théo Poizat <hello@zatteo.com>
2023-09-15 15:04:39 +0200

    feat: Add flagship app compatible storage wrapper
    It is almost and copy paste of storage.ts on flagship app. It will
    allow to have a storage API identical to flagship app storage API.
    When the big day of moving definitively this algorithm to the
    flagship app will come, it will be very easy.

---
+++ src/libs/localStorage/storage.ts
@@ -0,0 +1,54 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const { setItem, getItem, removeItem } = AsyncStorage
+export enum StorageKeys {
+  AutoLockEnabled = '@cozy_AmiralApp_autoLockEnabled',
+  BiometryActivated = '@cozy_AmiralApp_biometryActivated',
+  Capabilities = '@cozy_AmiralApp_Capabilities',
+  LastActivity = '@cozy_AmiralApp_lastActivity',
+  DefaultRedirectionUrl = '@cozy_AmiralAppDefaultRedirectionUrl',
+  SessionCreatedFlag = 'SESSION_CREATED_FLAG',
+  IconsTable = '@cozy_AmiralAppIcons'
+}
+
+export type IconsCache = Record<string, { version: string; xml: string }>
+
+export interface StorageItems {
+  biometryActivated: boolean
+  sessionCreatedFlag: string
+  iconCache: IconsCache
+}
+
+export const storeData = async (
+  name: StorageKeys,
+  value: StorageItems[keyof StorageItems]
+): Promise<void> => {
+  try {
+    await setItem(name, JSON.stringify(value))
+  } catch (error) {
+    console.log(`Failed to store key "${name}" to persistent storage`, error)
+  }
+}
+
+export const getData = async <T>(name: StorageKeys): Promise<T | null> => {
+  try {
+    const value = await getItem(name)
+
+    return value !== null ? (JSON.parse(value) as T) : null
+  } catch (error) {
+    console.log(`Failed to get key "${name}" from persistent storage`, error)
+    return null
+  }
+}
+
+export const clearData = async (): Promise<void> => {
+  try {
+    const keys = Object.values(StorageKeys)
+
+    for (const key of keys) {
+      await removeItem(key)
+    }
+  } catch (error) {
+    log.error(`Failed to clear data from persistent storage`, error)
+  }
+}
