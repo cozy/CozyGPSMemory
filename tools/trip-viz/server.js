39afba33f705246118a4e95694573fb297a433c2
paultranvan <paul@cozycloud.cc>
2023-09-27 17:07:12 +0200

    chore: Add express server for trip viz

--- tools/trip-viz/server.js
+++ tools/trip-viz/server.js
@@ -1,11 +1,10 @@
-
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const PORT = 3000
 
 // Servir les fichiers statiques du dossier 'public'
-app.use(express.static('public'));
+app.use(express.static('tools/trip-viz/public'))
 
 app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
-});
+  console.log(`Server started on http://localhost:${PORT}`)
+})
