82ccc6a39388912c28d70ec79100f3350f2b43da
paultranvan <paul@cozycloud.cc>
2023-09-27 16:00:24 +0200

    chore: Add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
-};
+  setupFiles: ['<rootDir>/__tests__/jestSetupFile.js'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?|p-timeout?|p-wait-for?|@notifee?)/)'
+  ],
+  rootDir: '.',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)']
+}
