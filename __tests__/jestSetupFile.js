d3eea73a5209ee8db92e35e03d744c84a9a13fa1
paultranvan <paul@cozycloud.cc>
2023-09-27 16:00:24 +0200

    chore: Add jest config

---
+++ __tests__/jestSetupFile.js
@@ -0,0 +1,6 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock'
+
+jest.mock('react-native-device-info', () => mockRNDeviceInfo)
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
