33f5d247ad410798998eff750d1acdb9f1fdfcdc
Théo Poizat <hello@zatteo.com>
2023-10-12 11:00:41 +0200

    fix: Enable minilog to get log in debug mode

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { AppRegistry } from 'react-native'
 import BackgroundGeolocation from 'react-native-background-geolocation'
 
+import Minilog from 'cozy-minilog'
+
 import App from './App'
 import { name as appName } from './app.json'
 import { GeolocationTrackingHeadlessTask } from './cozy-flagship-app/src/app/domain/geolocation/tracking/headless'
 
+Minilog.enable()
 AppRegistry.registerComponent(appName, () => App)
 
 BackgroundGeolocation.registerHeadlessTask(GeolocationTrackingHeadlessTask)
