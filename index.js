319a438ae55b369393786f8d162fca16f30af63c
Théo Poizat <hello@zatteo.com>
2023-10-12 10:59:34 +0200

    fix: Import headless task from submodule instead of deprecated code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import BackgroundGeolocation from 'react-native-background-geolocation'
 
 import App from './App'
 import { name as appName } from './app.json'
-import { GeolocationTrackingHeadlessTask } from './geolocation/services'
+import { GeolocationTrackingHeadlessTask } from './cozy-flagship-app/src/app/domain/geolocation/tracking/headless'
 
 AppRegistry.registerComponent(appName, () => App)
 
